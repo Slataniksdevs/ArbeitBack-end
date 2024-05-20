eb9f1633122bbf966e5c6ce93cfe091e69fe6d1c
juniordeza25 <juniordezaramos@gmail.com>
2024-05-20 16:09:25 -0400

    Modulos para encriptar contraseña, jwb y cookies, refactorizando el codigo de bd

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 require("dotenv").config(); // Obtener variables de entorno
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
-const connection = require("express-myconnection");
-const usersRoutes = require("./routes/usersRoutes");
+const cookieParser = require("cookie-parser");
+const v1UserRouter = require("./routes/usersRoutes");
 
 // Crear servidor
 const app = express();
 
 // Configuraciones
-const port = process.env.PORT ?? 3000;
-const dbSettings = {
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DATABASE,
-};
+const PORT = process.env.PORT ?? 3000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(connection(mysql, dbSettings, "single"));
+app.use(cookieParser());
+// Para eliminar el cache y no se pueda volver con el botón back luego de hacer logout
+/* app.use(function(req, res, next) {
+  if(!req.user)
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+  next()
+}) */
 
 // Rutas
-app.use("/", usersRoutes);
+app.use("/", v1UserRouter);
 
 // Escuchando el servidor
-app.listen(port, () => {
-  console.log(`Listening to server on port: http://127.0.0.1:${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening to server on port: http://127.0.0.1:${PORT}`);
 });
