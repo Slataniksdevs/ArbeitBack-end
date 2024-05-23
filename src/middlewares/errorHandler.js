41a5345640de0cb2d4400ca2883507eb44c5f88a
juniordeza25 <juniordezaramos@gmail.com>
2024-05-23 16:18:16 -0400

    Manejar codigo de error de credenciales no válidas

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,15 @@
 // Manejar los errores globales
 function errorHandler(err, req, res, next) {
-  console.error(err.stack);
+  console.error(err.stack); // Para depuración en el servidor
+
+  if (err.message === "Error al iniciar sesión: Correo electrónico incorrecto")
+    return res.status(401).json({
+      error: {
+        message:
+          err.message || "Error al iniciar sesión. Credenciales no válidas",
+      },
+    });
+
   res.status(err.status || 500).json({
     error: {
       message: err.message || "Error interno del servidor",
